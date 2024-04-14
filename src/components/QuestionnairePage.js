a4d70dcdb470d23abb14b2d48bbeb58b823aa1f0
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-04-14 02:33:23 -0500

    Added frontend

---
+++ src/components/QuestionnairePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const QuestionnairePage = () => {
+  return (
+    <div>
+      <h1>Questionnaire Page</h1>
+      {/* Add your questionnaire UI and functionality here */}
+    </div>
+  );
+};
+
+export default QuestionnairePage;
